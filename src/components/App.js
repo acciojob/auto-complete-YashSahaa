29a050cf627794dd4db9963a2902b6fdb4c838bc
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-07-10 11:31:01 +0530

    submission 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
 const App = () => {
+  const [getText,setGetText] = useState("");
+  const [textItems,setTextItems] = useState(fruits);
+
+  const handleChange = (e)=>{
+    const inputValue = e.target.value;
+    setGetText(inputValue);
+    const items = fruits.filter((fruit) => fruit.includes(inputValue))
+    setTextItems([...items]);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Search Item</h1>
+        <input type="text" value={getText} onChange={handleChange} />
+        <ul>
+          {textItems.map((textItem)=>{
+            return(
+              <li key={textItem}>{textItem}</li>
+            )
+          })}
+        </ul>
     </div>
   )
 }
